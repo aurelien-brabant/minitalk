2e85cef004e869608bec15be7c57c9ffb5077e7d
abrabant <abrabant@student.42.fr>
2021-06-17 14:20:31 +0200

    Add minitalk client, first version, untested

--- include/minitalk/client.h
+++ include/minitalk/client.h
@@ -1,3 +1,7 @@
 #ifndef CLIENT_H
 # define CLIENT_H
+# define ERROR_INVALID_PID "minitalk-client: invalid pid\n"
+# define ERROR_INVALID_PID_LEN 29
+# define ERROR_USAGE "minitalk-client: usage: client <server pid> <string>\n"
+# define ERROR_USAGE_LEN 53
 #endif
--- src/client/main.c
+++ src/client/main.c
@@ -1,8 +1,71 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <limits.h>
 
 #include "minitalk/client.h"
 
-int main(void)
+static int	parse_pid(const char *s)
 {
-	printf("client\n");
+	int	sign;
+	int	pid;
+
+	sign = 1;
+	pid = 0;
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			sign = -1;
+		++s;
+	}
+	while (*s >= '0' && *s <= '9')
+		pid = pid * 10 + (*s++ - 48);
+	if (*s != '\0')
+		return (-1);
+	return (pid * sign);
+}
+
+/*
+** SIG_USR2 is used to pass bits that are set.
+** SIG_USR1 is used to pass bits that are not set.
+**
+** The server will rebuild the string on its own using these two
+** signals.
+*/
+
+static void	send_string_to_server(int pid, const char *s)
+{
+	int	i;
+
+	while (*s != '\0')
+	{
+		i = 0;
+		while (i < CHAR_BIT)	
+		{
+			if (*s & (1 << i))
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			++i;
+		}
+		++s;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int	pid;
+
+	if (argc != 3)
+	{
+		write(STDERR_FILENO, ERROR_USAGE, ERROR_USAGE_LEN);
+		return (1);
+	}
+	pid = parse_pid(argv[1]);
+	if (pid == -1)
+	{
+		write(STDERR_FILENO, ERROR_INVALID_PID, ERROR_INVALID_PID_LEN);
+		return (2);
+	}
+	send_string_to_server(pid, argv[2]);
 }
